e5cbfb78bc0a66d0d6fb382f616c2a4485a7ef85
Eduardo Hartz <80062347+eduardohartz@users.noreply.github.com>
2024-12-13 19:10:23 -0800

    fix sum stuff

--- src/components/PageTransition.tsx
+++ src/components/PageTransition.tsx
@@ -1,5 +1,6 @@
 import { motion } from "framer-motion";
 import React from "react";
+import Loading from "./Loading";
 
 const pageVariants = {
   initial: { opacity: 0, x: 0 }, //x 100
@@ -16,11 +17,33 @@ const pageTransition = {
   mass: 1,
 };
 
+const loadingTransition = {
+  type: "tween",
+  duration: 0.1,
+  ease: "easeInOut",
+  stiffness: 100,
+  damping: 20,
+  mass: 1,
+};
+
 interface PageTransitionProps {
   children: React.ReactNode;
 }
 
 const PageTransition: React.FC<PageTransitionProps> = ({ children }) => {
+  if (React.isValidElement(children) && children.type === Loading) {
+    return (
+      <motion.div
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        variants={pageVariants}
+        transition={loadingTransition}
+      >
+        {children}
+      </motion.div>
+    );
+  }
   return (
     <motion.div
       initial="initial"
--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { JSX, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { User, Club } from '../types/models';
 
