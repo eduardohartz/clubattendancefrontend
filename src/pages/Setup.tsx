c8c467d50ca65eee64dbc1114bf07860ec47f25f
Eduardo Hartz <80062347+eduardohartz@users.noreply.github.com>
2024-12-19 09:39:03 -0800

    Update Setup.tsx

--- src/pages/Setup.tsx
+++ src/pages/Setup.tsx
@@ -170,20 +170,20 @@ function Setup() {
                         <div>
                             <button
                                 type="submit"
-                                className="flex w-full justify-center rounded-md bg-bg-accent-100 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-accent-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-accent-200 transition-colors"
+                                className="bg-accent-100 lex w-full justify-center rounded-md bg-bg-accent-100 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-accent-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-accent-200 transition-colors"
                                 disabled={isSubmitting}
                             >
 
                                 {isSubmitting
                                     ? (
-                                            <svg className="animate-spin h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                                                <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-                                                <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                                            </svg>
-                                        )
+                                        <svg className="animate-spin h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                            <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                                            <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                                        </svg>
+                                    )
                                     : (
-                                            "Setup"
-                                        )}
+                                        "Setup"
+                                    )}
 
                             </button>
                         </div>
